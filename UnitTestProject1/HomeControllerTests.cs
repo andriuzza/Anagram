5c8bfc4096ae8f0a8791608d96e8174d1752e541
Andrius aa <andrius.butkevicius1@gmail.com>
2018-02-07 23:41:55 +0200

    new unit testing

---
+++ UnitTestProject1/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace WebUnitTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_EmptyQuery_ReturnsEmptyView()
+        {
+            // Create
+            var homeController = new HomeController(null);
+
+            // Act
+            var result = homeController.Index("") as ViewResult;
+
+            // Assert
+            Assert.AreEqual("", result.ViewName);
+        }
+        [TestMethod]
+        public void Index_QueryOfOneWord_ReturnsSomething()
+        {
+            // Create
+            var homeController = new HomeController(null);
+
+            // Act
+            var result = homeController.Index("vaidenasi") as ViewResult;
+
+            // Assert
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
